24261dc24b029a294dc3be9c32a615b1a3d389d5
Thant Zin Oo <tzoo2024@gmail.com>
2026-05-23 23:30:06 +0630

    Refactor KanoteBackground component styles
    Updated opacity and transition properties for Kanote images.

--- src/components/KanoteBackground.jsx
+++ src/components/KanoteBackground.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-
 import kanoteTopRight from '../assets/kanote-style-2.png'; 
 import kanoteBottomLeft from '../assets/kanote-style-1.png'; 
 
@@ -8,20 +6,23 @@ const KanoteBackground = () => {
   return (
     <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden select-none">
       
-      
-<div className="absolute top-15 -right-20 w-87 h-87"> 
-  <img 
-    src={kanoteTopRight} 
-    alt="kanote decorative ornament top right" 
-    className="w-full h-full object-contain opacity-10 dark:opacity-30 transition-opacity duration-500" 
-  />
-</div>
+      {/* Top Right Kanote */}
+      <div className="absolute top-15 -right-20 w-87 h-87"> 
+        <img 
+          src={kanoteTopRight} 
+          alt="kanote decorative ornament top right" 
+          
+          className="w-full h-full object-contain opacity-10 dark:opacity-20 dark:invert transition-all duration-500" 
+        />
+      </div>
 
+      {/* Bottom Left Kanote */}
       <div className="absolute -bottom-10 -left-42 w-150 h-150">
         <img 
           src={kanoteBottomLeft} 
           alt="kanote decorative ornament bottom left" 
-          className="w-full h-full object-contain opacity-1 dark:opacity-50 transition-opacity duration-500" 
+         
+          className="w-full h-full object-cover opacity-5 dark:opacity-15 dark:invert transition-all duration-500" 
         />
       </div>
 
